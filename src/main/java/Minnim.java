6976d053225880aaf24fdc76d0dbb1cd14972ba5
cscms03 <choi.m4527@gmail.com>
2025-01-23 17:54:24 +0800

    Add echo feature

--- src/main/java/Minnim.java
+++ src/main/java/Minnim.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Minnim {
+    private Scanner s = new Scanner(System.in);
+
+    private void chat(){
+        String message = s.nextLine();
+        if (message.equals("bye")) {
+            System.out.println("     Bye. Hope to see you again soon!");
+        } else {
+            System.out.println("    "+ message);
+            chat();
+        }
+    }
     public static void main(String[] args) {
+
+        Minnim bot = new Minnim();
+
+        //greetings
         System.out.println( "Hello! I'm Minnim.");
         System.out.println( "What can I do for you?");
         System.out.println( "\n");
-        System.out.println( "Bye. Hope to see you again soon!");
+        //echo
+        bot.chat();
+
     }
 }
