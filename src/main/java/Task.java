fc1ed2ff1403a803bb23586b19a29f638bf69529
cscms03 <choi.m4527@gmail.com>
2025-01-23 19:31:45 +0800

    Modify Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Task {
+    protected String description;
+    protected boolean isDone;
+
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public String getDescription() {
+        return  ". " + "[" + getStatusIcon() + "]" + this.description;
+    }
 }
