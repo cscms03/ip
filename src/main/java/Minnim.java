3fc8cc07a14a27c31c9c26377ef89ba72f47a84c
cscms03 <choi.m4527@gmail.com>
2025-01-23 19:08:02 +0800

    Add Adding and List feature

--- src/main/java/Minnim.java
+++ src/main/java/Minnim.java
@@ -2,14 +2,27 @@ import java.util.Scanner;
 
 public class Minnim {
     private Scanner s = new Scanner(System.in);
+    private String[] tasks = new String[100];
 
-    private void chat(){
-        String message = s.nextLine();
-        if (message.equals("bye")) {
-            System.out.println("     Bye. Hope to see you again soon!");
-        } else {
-            System.out.println("    "+ message);
-            chat();
+    private void chat() {
+        int i = 0;
+        while (true) {
+            String message = s.nextLine();
+            if (message.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                break;
+            }
+            if (message.equals("list")) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(j + 1 + ". " + this.tasks[j]);
+                    System.out.print("\n");
+                }
+            } else {
+                this.tasks[i] = message;
+                System.out.println("added: " + message);
+                System.out.print("\n");
+                i++;
+            }
         }
     }
     public static void main(String[] args) {
@@ -20,7 +33,8 @@ public class Minnim {
         System.out.println( "Hello! I'm Minnim.");
         System.out.println( "What can I do for you?");
         System.out.println( "\n");
-        //echo
+
+        //execute chat
         bot.chat();
 
     }
