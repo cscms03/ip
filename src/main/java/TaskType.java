835e3336f6042e263d7b1a594f84c0a267394fa0
cscms03 <choi.m4527@gmail.com>
2025-01-24 14:41:35 +0800

    Add enum for refactoring

---
+++ src/main/java/TaskType.java
@@ -0,0 +1,28 @@
+public enum TaskType {
+    TODO("todo"),
+    DEADLINE("deadline"),
+    EVENT("event"),
+    MARK("mark"),
+    UNMARK("unmark"),
+    DELETE("delete"),
+    LIST("list");
+
+    private final String keyword;
+
+    TaskType(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public String getKeyword() {
+        return this.keyword;
+    }
+
+    public static TaskType fromString(String keyword) {
+        for (TaskType type : TaskType.values()) {
+            if (type.getKeyword().equalsIgnoreCase(keyword)) {
+                return type;
+            }
+        }
+        return null; // If no match is found
+    }
+}
