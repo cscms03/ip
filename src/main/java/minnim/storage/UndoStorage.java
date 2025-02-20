350f210a56438ee120bf30ac55757eb766d86ab7
cscms03 <choi.m4527@gmail.com>
2025-02-20 14:13:05 +0800

    Add UndoStorage class Stores recent tasks done, or deleted task if applicable.

---
+++ src/main/java/minnim/storage/UndoStorage.java
@@ -0,0 +1,35 @@
+package minnim.storage;
+
+import java.util.Stack;
+
+import minnim.task.Task;
+
+public class UndoStorage {
+    private Stack<String> recentTasks = new Stack<>();
+    private Stack<Task> deletedTasks = new Stack<>();
+    private static final int MAX_UNDO = 1;
+
+    public void addRecentTask(String message) {
+        if (recentTasks.size() == MAX_UNDO) {
+            recentTasks.remove(0); // Maintain size limit
+        }
+        recentTasks.push(message);
+        System.out.println(message + recentTasks.toString());
+    }
+
+    public void storeDeletedTask(Task task) {
+        if (deletedTasks.size() == MAX_UNDO) {
+            deletedTasks.remove(0); // Maintain size limit
+        }
+        deletedTasks.push(task);
+        System.out.println(task.getDescription());
+    }
+
+    public Task getDeletedTask() {
+        return deletedTasks.isEmpty() ? null : deletedTasks.pop();
+    }
+
+    public String getRecentTask() {
+        return recentTasks.isEmpty() ? null : recentTasks.pop();
+    }
+}
