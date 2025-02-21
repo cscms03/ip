d191c8a0a25ccf2d5b8b1031a3f64a9dcd1a04e9
cscms03 <choi.m4527@gmail.com>
2025-02-22 01:15:35 +0800

    Remove unused methods in Minnim class

--- src/main/java/minnim/Minnim.java
+++ src/main/java/minnim/Minnim.java
@@ -37,40 +37,10 @@ public class Minnim {
         this.undoStorage = new UndoStorage();
         this.tasks = new TaskList(storage.loadTasks(), ui, undoStorage);
         this.parser = new Parser(tasks, ui, storage, undoStorage);
-
-        ui.showMessage("Below is the current task list:");
-        tasks.listTasks();
-    }
-
-    /**
-     * Runs the Minnim application, processing user input and executing commands.
-     * The method continuously accepts user input from the console, passes the input to the parser,
-     * and executes corresponding actions until the program is terminated.
-     *
-     *
-     * @throws MinnimMissingTaskDetailException if a task detail is missing during command parsing
-     * @throws MinnimMissingDateException if a date is missing during command parsing
-     * @throws MinnimTargetTaskNumNotFoundException if the task number is not found during marking or deleting
-     * @throws MinnimNoTaskFoundException if no task is found with the specified task number
-     */
-    public void run() throws MinnimMissingTaskDetailException, MinnimMissingDateException,
-            MinnimTargetTaskNumNotFoundException, MinnimNoTaskFoundException {
-        ui.showWelcomeMessage();
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-
-        while (true) {
-            String input = scanner.nextLine();
-            System.out.println(parser.parseCommand(input));
-        }
     }
 
     public String getResponse(String message) throws MinnimMissingTaskDetailException, MinnimMissingDateException,
             MinnimTargetTaskNumNotFoundException, MinnimNoTaskFoundException {
         return parser.parseCommand(message);
-    }
-
-    public static void main(String[] args) throws MinnimMissingTaskDetailException, MinnimMissingDateException,
-            MinnimTargetTaskNumNotFoundException, MinnimNoTaskFoundException {
-        new Minnim("data/minnim.Minnim.txt").run();
     }
 }
